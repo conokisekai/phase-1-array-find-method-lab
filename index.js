19561cd35837c61ca73e62fc1b639eeec221ae06
Conrad Kambi <kambiconrad@gmail.com>
2023-10-12 16:23:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code your solution here
+function superbowlWin(superbowlArray) {
+    // Using the find() method to search for a win ("W") in the array
+    const winningYearObject = superbowlArray.find(superbowl => superbowl.result === 'W');
+  
+    // If a win is found, return the year, else return undefined
+    return winningYearObject ? winningYearObject.year : undefined;
+  }
